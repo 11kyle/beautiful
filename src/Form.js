1a0d6bdde83bbed78cdde608593f00862928a09e
Kyle <kyle@Kyles-MacBook-Pro.local>
2019-01-02 18:29:48 -0700

    First Commit

---
+++ src/Form.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  handleSubmit(event) {
+    alert(this.state.value + ', you are beautiful!');
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input id="name" type="text" name="name" value={this.state.value} onChange={this.handleChange} className="form-control" />
+        </div>
+        <button type="submit" className="btn btn-success">Submit</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
